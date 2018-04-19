7f96ef8ea05d123d82c809677a594c0086db0716
edi <edi@CC201-31.itam.mx>
2018-04-19 16:25:42 -0500

    Casi final

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -10,234 +10,142 @@ package sudoku;
  * @author FARAMBURA
  */
 public class Sudoku {
-
-    
     
     int sudoku[][];
-
-   
+    
     public Sudoku() {
-        
         sudoku= new int [9][9];
-        
     }
     
     public Sudoku(int[][] sudoku) {
         this.sudoku = sudoku;
-    
     }
     
     private boolean resCol(int col, int num){
-        
         boolean res = true;
-        
         for(int i = 0; i<9; i++){
-            
             if(sudoku[i][col] == num){
                 res = false;
-                
             }
         }
         return res;
-        
-    
     }
-     
+    
     private boolean resFila(int fil, int num){
-        
         boolean res = true;
-        
         for(int i = 0; i<9; i++){
-            
-            if(sudoku[fil][i] == num){
+            if(sudoku[fil][i] == num)
                 res = false;
-                
-            }
         }
         return res;
-        
-    
     }
      
     private  boolean cuadro(int fila, int col, int num){
-        
         boolean res = true;
-        
         for(int i = fila; i<fila+3; i++){
             for(int k = col; k < col+3; k++){
-                if(sudoku[i][k] == num){
+                if(sudoku[i][k] == num)
                     res = false;
-                }
             }
         }
         return res;
-        
-    
     }
     
     public boolean valida(int fila, int columna, int num){
-    
         boolean res = false;
-        
         int col, fil;
-        
-        
-        if(columna<3){
+        if(columna<3)
             col = 0;
-        }
-        else if(columna<6){
+        else if(columna<6)
             col = 3;
-        }
-        else{
+        else
             col = 6;
-        }
-        
-        
-        if(fila<3){
+        if(fila<3)
             fil = 0;
-        }
-        else if(fila<6){
+        else if(fila<6)
             fil = 3;
-        }
-        else{
+        else
             fil = 6;
-        }
-        
-        if(resCol(columna , num) && resFila(fila, num) && cuadro(fil, col, num)){
+        if(resCol(columna , num) && resFila(fila, num) && cuadro(fil, col, num))
             res = true;
-        }
-        
         return res;
-    
-    
-    
     }
     
     public boolean resuelve(){
-        
         for(int f =0; f<9 ; f++){
             for(int c = 0; c < 9 ; c++){
                 if(sudoku[f][c] == 0){
                     for(int n =1; n<10 ; n++){
                         if(valida(f,c,n)){
                             sudoku[f][c] = n;
-                            if(resuelve()){
+                            if(resuelve())
                                 return true;
-                            }
-                            else{
+                            else
                                 sudoku[f][c] = 0;
-                            }
-                        
                         }
-                    
-                    
                     }
-                return false;
-                
-                
+                    return false;
                 }
-        
-        
             }
         }
         return true;
-        
-        
-        
-        
     }
     
     public boolean revisionInicial(){
-        
         boolean res = true;
         int aux;
-        
         if(revisionNumerosValidos()){
-            
             for(int f = 0; f<9; f++){
-                
                 for(int c = 0; c<9 ; c++){
-                
                     if(sudoku[f][c] != 0){
-                        
                         aux = sudoku[f][c];
                         sudoku[f][c] = 0;
-                        
-                        if(!valida(f,c,aux)){
+                        if(!valida(f,c,aux))
                             res = false;
-                        }
                         else
-                          sudoku[f][c] = aux;  
+                          sudoku[f][c] = aux;
                     }
-                
                 }
-            
             }
-        
         }
-        else{
+        else
             res=false;
-        }
-        
-        
-     return res;   
+        return res;
     }
     
     public boolean revisionNumerosValidos(){
-    
         boolean res = true;
-        
         for(int f = 0; f<9; f++){
-            
             for(int c = 0; c<9 ; c++){
-                
                 if(sudoku[f][c]<0 || sudoku[f][c]>10){
                     res = false;
                 }
-                
             }
-            
         }
-        
         return res;
-        
-    
-        
     }
     
     
     
     
     
-     public  void imprimeSudoku(){
-        
+    public  void imprimeSudoku(){
         String res = "";
-        
         for(int i =0; i<9; i++){
             for(int k = 0; k<9 ; k++){
-                
-                if(k==3 || k == 6 ){
+                if(k==3 || k == 6 )
                     res = res + " " + "|" + " ";
-                }
-                
-                
                 res = res + " " + sudoku[i][k];
-                
             }
-           
-            if(i==2 || i==5){
+            if(i==2 || i==5)
                     res = res + "\n " + "------------------------" + "\n";
-            }
             else
                  res = res + "\n";
         }
         System.out.println(res);
-        
-        
     }
-     
+    
     public int[][] regresaSudoku(){
-        
         return sudoku;
     } 
  
